94b388ee260e5cd3a848a630e40626217614f532
Mason Mark <mason@fivespeed.com>
2012-01-17 14:09:50 +0900

    OK: Add NakaharaFoundation, still ok.

--- XcodeTest/XcodeBugViewController.h
+++ XcodeTest/XcodeBugViewController.h
@@ -10,17 +10,24 @@
 
 @interface XcodeBugViewController : NSViewController
 {
-    IBOutlet NSView *out10b;
-    IBOutlet NSView *out10;
-    IBOutlet NSView *out9;
-    IBOutlet NSView *outlet8;
-    IBOutlet NSView *outlet7;
-    IBOutlet NSView *outlet1;
-    IBOutlet NSView *outlet2;
-    IBOutlet NSView *outlet3;
-    IBOutlet NSView *outlet4;
-    IBOutlet NSView *outlet5;
-    IBOutlet NSView *outlet6;
+    IBOutlet NSView *hellbane;
+    IBOutlet NSView *nerdbergf;
+    IBOutlet NSView *donkdonk;
+    IBOutlet NSView *bob;
+    IBOutlet NSView *alice;
+    IBOutlet NSView *nerdbergffff;
+
+//    IBOutlet NSView *out10b;
+//    IBOutlet NSView *out10;
+//    IBOutlet NSView *out9;
+//    IBOutlet NSView *outlet8;
+//    IBOutlet NSView *outlet7;
+//    IBOutlet NSView *outlet1;
+//    IBOutlet NSView *outlet2;
+//    IBOutlet NSView *outlet3;
+//    IBOutlet NSView *outlet4;
+//    IBOutlet NSView *outlet5;
+//    IBOutlet NSView *outlet6;
 
 }
 
